c335c20495b274be282b2ebe9108d4f1f948a7a4
rifqi <rifqi@gmail.com>
2023-12-15 21:02:03 +0700

    Final

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', [AuthController::class,'login']);
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::get('/books',[BookController::class,'books']);
+    
+    Route::post('/books',[BookController::class,'store']);
+    Route::match(['put','patch'],'/books/{id}',[BookController::class,'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
 });
